84ace3768bd3278ddfd85ef37c1f49de6f1bb37c
Matthews Ma <matthewsma21@gmail.com>
2025-01-27 19:49:28 +0900

    Process each demo individually

--- README.md
+++ README.md
@@ -5,14 +5,24 @@ Download [Python](https://www.python.org/downloads/). I'm using version 3.10, bu
 
 Open the `monkeys-data-calculations` folder in a command line. One way to do this is to open the folder in Windows explorer, then `SHIFT + RIGHT CLICK` on an empty space. Then click "Open PowerShell window here".
 
-Run the following commands to set up the environment:
+Run the following commands **one time** depending on your system to set up the environment:
+
+Windows with CMD or PowerShell
 ```bash
 python -m venv venv
 venv/Scripts/activate.bat
 pip install -r requirements.txt
 ```
 
-Now run the script with your folder path as an argument:
+Mac with Terminal
 ```bash
+python -m venv venv
+source venv/Scripts/activate
+pip install -r requirements.txt
+```
+
+Now run the script with your folder path as an argument. You must reactivate the environment every time you open a new terminal:
+```bash
+source venv/Scripts/activate
 python calculate_times.py data_folder/*
 ```
--- calculate_times.py
+++ calculate_times.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import argparse
-import pathlib
 
 OBSERVATION_ID_COL_NAME = "Observation id"
 BEHAVIOUR_COL_NAME      = "Behavior"
 DURATION_COL_NAME       = "Duration (s)"
+START_COL_NAME          = "Start (s)"
+STOP_COL_NAME           = "Stop (s)"
 DEMO_BEHAVIOUR_NAME     = "Demo"
 LOOK_BEHAVIOUR_NAME     = "Look"
+
+
+### DURATIONS CALCULATIONS ###
 
 def calculate_looks_and_demos(filepath: str) -> pd.DataFrame:
     df = pd.read_excel(filepath)
@@ -56,9 +60,64 @@ def report_looks_and_demos_multiple(filepaths: list[str]) -> None:
     print("------------------------------")
 
 
+### INDIVIDUAL DEMOS CALCULATIONS ###
+
+def calculate_individual_looks_and_demos(filepath: str) -> pd.DataFrame:
+    df = pd.read_excel(filepath)
+    demo_intervals = df[df[BEHAVIOUR_COL_NAME] == DEMO_BEHAVIOUR_NAME].reset_index(drop=True)
+    look_intervals = df[df[BEHAVIOUR_COL_NAME] == LOOK_BEHAVIOUR_NAME].reset_index(drop=True)
+    
+    demo_intervals["Demo Number"] = demo_intervals.groupby(OBSERVATION_ID_COL_NAME).cumcount() + 1
+
+    results = []
+    for i, demo in demo_intervals.iterrows():
+        id = demo[OBSERVATION_ID_COL_NAME]
+        index = demo["Demo Number"]
+        start = demo[START_COL_NAME]
+        stop = demo[STOP_COL_NAME]
+        duration = stop - start
+
+        overlapping_looks = look_intervals[
+            (look_intervals[OBSERVATION_ID_COL_NAME] == id) &
+            (look_intervals[START_COL_NAME] < stop) &
+            (look_intervals[STOP_COL_NAME] > start)
+        ]
+
+        total_look_time = 0
+        for i, look in overlapping_looks.iterrows():
+            s = max(start, look[START_COL_NAME])
+            e = min(stop, look[STOP_COL_NAME])
+            total_look_time += e - s
+        percent = total_look_time / duration * 100
+
+        results.append({
+            OBSERVATION_ID_COL_NAME: id,
+            "Demo Number": index,
+            "Look/Demo %": percent,
+            "Total Look Time": total_look_time,
+            "Total Demo Time": duration,
+        })
+
+    return pd.DataFrame(results)
+
+
+def calculate_individual_looks_and_demos_multiple(filepaths: list[str]) -> pd.DataFrame:
+    file_results = []
+    for filepath in filepaths:
+        file_results.append(calculate_individual_looks_and_demos(filepath))
+        print("Finished processing file", filepath)
+
+    result_df = pd.concat(file_results)
+
+    return result_df
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Calculate times for monkey data.")
-    parser.add_argument('files', nargs="*", help="File path(s) to excel sheet (aggregated format).")
+    parser.add_argument("files", nargs="*", help="File path(s) to excel sheet (aggregated format).")
+    parser.add_argument("--output", default="output.xlsx", help="Output file path (default: output.xlsx)")
     args = parser.parse_args()
         
-    report_looks_and_demos_multiple(args.files)
+    df = calculate_individual_looks_and_demos_multiple(args.files)
+    df.to_excel(args.output, index=False)
+    print("Exported results to", args.output)
